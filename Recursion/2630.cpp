1c453ce658867b65d60871342010928509147c8f
Donghyeon Seo <ehdgus18320@gmail.com>
2023-03-12 00:08:57 +0900

    2023.03.12

--- Recursion/2630.cpp
+++ Recursion/2630.cpp
@@ -1,38 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+int result[2]; //result[0] = 0 개수, result[1] = 1 개수
+int board[130][130];
 
-string board[66];
-
-void rec(int x_start, int x_end, int y_start, int y_end)
+void rec(int x_start, int x_end, int y_start, int y_end, int num)
 {
 	if(x_start + 1 == x_end)
 	{
-		cout << board[x_start][y_start];
+		if (board[x_start][y_start] == num)
+			result[num]++;
 		return;
 	}
 	int check = 1;
-	int sum = 0;	//전부 0일때
-	int max_sum = (x_end - x_start) * (y_end - y_start); // 전부 1일 때
 	for (int i = x_start; i < x_end; i++)
 	{
 		for (int j = y_start; j < y_end; j++)
 		{
-			sum += board[i][j] - '0';
+			if (board[i][j] != num)
+				check = 0;
 		}
 	}
-	if (sum == 0 || sum == max_sum)
+	if (check == 1)
 	{
-		cout << board[x_start][y_start];
+		result[num]++;
 		return;
 	}
-	cout << '(';
 	int x_half = x_start + (x_end - x_start) / 2;
 	int y_half = y_start + (y_end - y_start) / 2;
-	rec(x_start, x_half, y_start, y_half);
-	rec(x_start, x_half, y_half, y_end);
-	rec(x_half, x_end, y_start, y_half);
-	rec(x_half, x_end, y_half, y_end);
-	cout << ')';
+	rec(x_start, x_half, y_start, y_half, num);
+	rec(x_start, x_half, y_half, y_end, num);
+	rec(x_half, x_end, y_start, y_half, num);
+	rec(x_half, x_end, y_half, y_end, num);
 }
 
 int main ()
@@ -41,8 +39,11 @@ int main ()
 	cin >> n;
 	for (int i = 0; i < n; i++)
 	{
-		cin >> board[i];
+		for (int j = 0; j < n; j++)
+			cin >> board[i][j];
 	}
-	rec(0, n, 0, n);
+	rec(0, n, 0, n, 0);
+	rec(0, n, 0, n, 1);
+	cout << result[0] << '\n' << result[1];
 	return 0;
 }
