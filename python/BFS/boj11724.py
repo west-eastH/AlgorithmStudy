6680c86a70ee016f53403a4daf5e883b65618b84
west_east <west.east1832@gmail.com>
2024-04-05 13:38:53 +0900

    [solved] : boj11724

--- python/BFS/boj11724.py
+++ python/BFS/boj11724.py
@@ -1,14 +1,33 @@
 from collections import deque
-
-def bfs(x, y) :
-	queue.append(graph[1])
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
 
-graph = []
-graph.append((0,0))
-for i in range(m):
-	graph.append(tuple(map(int, input().split())))
+for _ in range(m):
+	u, v = map(int, input().split())
+	graph[u].append(v)
+	graph[v].append(u)
+ 
+visited = [False] * (n + 1)
+count = 0
 
-queue = deque()
-print(bfs(graph[1]))
+def bfs(i):
+	queue = deque([i])
+	visited[i] = True
+	
+	while(queue):
+		v = queue.popleft()
+		for i in graph[v]:
+			if (visited[i] == False):
+				visited[i] = True
+				queue.append(i)
+
+
+for i in range(1, n + 1):
+	if (visited[i] == False):
+		count += 1
+		bfs(i)
+print(count)
