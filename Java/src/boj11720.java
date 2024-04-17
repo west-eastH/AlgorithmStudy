b85deb847bf224a29ce7c3cc04fe66ec8648e82e
west_east <west.east1832@gmail.com>
2024-04-17 15:16:23 +0900

    [solved] boj11720

---
+++ Java/src/boj11720.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj11720 {
+	private static int n, m;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(bf.readLine());
+		String st = bf.readLine();
+		int result = 0;
+		for (int i = 0; i < n; i++) {
+			result += st.charAt(i) - '0';
+		}
+		System.out.println(result);
+	}
+}
